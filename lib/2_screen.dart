193164d9151e59c98346b7eac829ae69b1c34820
Tgacky-tech <shota0037.takagaki@gmail.com>
2024-08-18 21:51:46 +0900

    sns、ブラウザ、wifi画面にsvg画像を挿入

--- lib/2_screen.dart
+++ lib/2_screen.dart
@@ -1,17 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class screen2 extends StatelessWidget {
   const screen2({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final appBar = AppBar(
-      backgroundColor: Colors.grey,
-      title: const Text('SNS'),
-    );
     return Scaffold(
-      appBar: appBar,
+      body: Center(
+        child: Container(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 27 / 2,
+                child: Container(
+                  color: Colors.blue,
+                ),
+              ),
+              AspectRatio(
+                aspectRatio: 27 / 46,
+                child: Container(
+                  child: SvgPicture.asset(
+                      'images/LinkUp.svg'
+                  ),
+                ),
+              ),
+              AspectRatio(
+                aspectRatio: 27 / 4,
+                child: Container(
+                  color: Colors.blue,
+                ),
+              )
+              // TextButton(
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/3_screen.dart
+++ lib/3_screen.dart
@@ -1,17 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class screen3 extends StatelessWidget {
   const screen3({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final appBar = AppBar(
-      backgroundColor: Colors.grey,
-      title: const Text('ブラウザ'),
-    );
     return Scaffold(
-      appBar: appBar,
+      body: Center(
+        child: Container(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 27 / 2,
+                child: Container(
+                  color: Colors.blue,
+                ),
+              ),
+              AspectRatio(
+                aspectRatio: 27 / 46,
+                child: Container(
+                  child: SvgPicture.asset(
+                      'images/news.svg'
+                  ),
+                ),
+              ),
+              AspectRatio(
+                aspectRatio: 27 / 4,
+                child: Container(
+                  color: Colors.blue,
+                ),
+              )
+              // TextButton(
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
