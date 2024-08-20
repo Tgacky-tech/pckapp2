e0cf0f5f8144f8dccc75d20c3499fd8edb0d4d9e
Tgacky-tech <shota0037.takagaki@gmail.com>
2024-08-20 12:27:36 +0900

    ゲーム内ホーム画面のデザイン作成

--- lib/0-0_screen.dart
+++ lib/0-0_screen.dart
@@ -11,31 +11,59 @@ class screen00 extends StatelessWidget {
       backgroundColor: Colors.yellow,
       title: const Text('0:00'),
     );
-    final pushButton1 = ElevatedButton(
+    final pushButton1 = MaterialButton(
       onPressed: () => context.push('/1'),
-      style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
-      child: const Text('メール'),
+      child: Container(
+        width: 60,
+        height: 60,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/mail.png'),
+              fit: BoxFit.cover,
+            )),
+    ),
     );
-    final pushButton2 = ElevatedButton(
+    final pushButton2 = MaterialButton(
       onPressed: () => context.push('/2'),
-      style: ElevatedButton.styleFrom(backgroundColor: Colors.yellow),
-      child: const Text('SNS'),
+      child: Container(
+        width: 60,
+        height: 60,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/sns.png'),
+              fit: BoxFit.cover,
+            )),
+      ),
     );
-    final pushButton3 = ElevatedButton(
+    final pushButton3 = MaterialButton(
       onPressed: () => context.push('/3'),
-      style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-      child: const Text('ブラウザ'),
+      child: Container(
+        width: 60,
+        height: 60,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/browsericon.png'),
+              fit: BoxFit.cover,
+            )),
+      ),
     );
-    final pushButton4 = ElevatedButton(
+    final pushButton4 = MaterialButton(
       onPressed: () => context.push('/4'),
-      style: ElevatedButton.styleFrom(backgroundColor: Colors.grey),
-      child: const Text('Wi-Fi'),
+      child: Container(
+        width: 60,
+        height: 60,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/settingsicon.png'),
+              fit: BoxFit.cover,
+            )),
+      ),
     );
-    final pushButton5 = ElevatedButton(
-      onPressed: () => context.push('/5'),
-      style: ElevatedButton.styleFrom(backgroundColor: Colors.grey),
-      child: const Text('バッテリー'),
-    );
+    // final pushButton5 = ElevatedButton(
+    //   onPressed: () => context.push('/5'),
+    //   style: ElevatedButton.styleFrom(backgroundColor: Colors.grey),
+    //   child: const Text('バッテリー'),
+    // );
     final pushButton6 = ElevatedButton(
       onPressed: () => context.push('/menu'),
       style: ElevatedButton.styleFrom(backgroundColor: Colors.grey),
@@ -50,27 +78,116 @@ class screen00 extends StatelessWidget {
       child: const Text('✖'),
     );
     return Scaffold(
-      appBar: appBar,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            pushButton1,
-            pushButton2,
-            pushButton3,
-            pushButton4,
-            pushButton5,
-            pushButton6,
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                pushButton7,
-                pushButton8,
-              ],
-            ),
-          ],
+    body: Center(
+    child: Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/background.png'),
+            fit: BoxFit.cover,
+          )),
+    child: Column(
+    mainAxisSize: MainAxisSize.min,
+    children: <Widget>[
+    AspectRatio(
+    aspectRatio: 27 / 1.5,
+    child: Container(
+      color: Color(0xFF636363),
+      child: Text(
+        '00:00',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Colors.white,
         ),
       ),
+    ),
+    ),
+    AspectRatio(
+    aspectRatio: 27 / 46,
+    child: Container(
+      alignment: Alignment.topCenter,
+      padding:EdgeInsets.symmetric(vertical: 10,),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              pushButton1,
+              Text(
+                'メール',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              pushButton2,
+              Text(
+                'sns',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              pushButton3,
+              Text(
+                'ブラウザー',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              pushButton4,
+              Text(
+                '設定',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ],
+        // children: <Widget>[
+        //   pushButton1,
+        //   pushButton2,
+        //   pushButton3,
+        //   pushButton4,
+        //   // pushButton5,
+        //
+        // ],
+      ),
+    ),
+    ),
+    AspectRatio(
+    aspectRatio: 27 / 4,
+    child: Container(
+    color: Colors.blue,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          pushButton6,
+          pushButton7,
+          pushButton8,
+        ],
+      ),
+    ),
+    )
+    // TextButton(
+    ],
+    ),
+    ),
+    ),
     );
   }
 }
