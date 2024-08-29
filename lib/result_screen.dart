c8f05eab4098c7d6c1255e87e82a44d7ce13ddf5
Tgacky-tech <shota0037.takagaki@gmail.com>
2024-08-29 14:24:23 +0900

    リザルト画面と異変いれといた

--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -1,26 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pckapp2/providers/stopwatch_provider.dart';
 
-class result_screen extends StatelessWidget {
-  const result_screen({super.key});
+class result_screen extends ConsumerWidget {
+  const result_screen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    const message = Text('クリアおめでとう');
-    final appBar = AppBar(
-      backgroundColor: Colors.red,
-      title: const Text('リザルト'),
-    );
+  Widget build(BuildContext context, WidgetRef ref) {
+    final stopwatchNotifier = ref.watch(stopwatchProvider.notifier);
     return Scaffold(
-      appBar: appBar,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            message,
-              ],
+            Text(
+              'クリアおめでとう！',
+              style: TextStyle(
+                fontSize: 30,
+              ),
             ),
+            Text(
+              'クリアタイム: ${stopwatchNotifier.formattedElapsedTime}',
+              style: TextStyle(
+                fontSize: 35,
+              ),
+            ),
+            Text(
+              '登場した異変一覧\nコレクションに保存されたよ！',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  context.push('/home');
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.yellow,
+                    fixedSize: const Size(200, 60)
+                ),
+                child: const Text('ホーム画面へ')
+            ),
+          ],
         ),
+      ),
     );
   }
 }
