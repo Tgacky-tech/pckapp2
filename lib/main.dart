3c93bd1902b6845979b8ca80cbd20c8770398d65
Tgacky-tech <shota0037.takagaki@gmail.com>
2024-09-20 16:56:42 +0900

    データベースすこし進みました

--- lib/main.dart
+++ lib/main.dart
@@ -17,10 +17,13 @@ import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pckapp2/providers/stopwatch_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     ProviderScope(
         child: MyApp()),
