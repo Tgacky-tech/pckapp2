098bcd5758791ec3a050104ddd11b2b8a6a68f95
SO\81809 <okumurasota@gmail.com>
2024-10-27 18:32:12 +0900

    いい方法が思い浮かばんかった

--- lib/collection_screen.dart
+++ lib/collection_screen.dart
@@ -146,11 +146,11 @@ class collection_screen extends StatelessWidget {
               fontStyle: FontStyle.italic,
             ),
           ),
+              Image.asset('images/nmail.png'),
               _dialog('1','焦らせるような文面でも、安易にリンクを開いてはいけません。URLを開いてしまうと、ウイルスがインストールされる可能性があります。'),
               _dialog('2', '迷惑メールは安易に開いてはいけません。メールアプリの送信元を拒否する設定を活用しましょう。'),
               _dialog('3', 'ウェブサイトに自分のアカウントでログインすると、登録されたメールアドレスに通知が送られてくることがあります。身に覚えのないログインの場合、アカウント情報が流出している可能性があります。パスワードを変更するなどの対策を取りましょう。'),
               _dialog('4', 'メールの添付ファイルを開いただけでも、デバイスがウイルスに感染する可能性があります。知り合いからのメールであっても、注意しましょう。'),
-              Text(''),
               Text(
                 'SNS',
                 style: TextStyle(
@@ -159,11 +159,13 @@ class collection_screen extends StatelessWidget {
                   fontStyle: FontStyle.italic,
                 ),
               ),
+              Image.asset('images/nsns.png'),
               _dialog('5', 'SNSでお金や商品を配ろうとする投稿は、注意しましょう。安易にリンクを開いたり、投稿を拡散してはいけません。'),
               _dialog('6', 'SNSでは個人情報を乗せるのはやめましょう。ユーザー名やプロフィール、投稿にも注意しましょう。'),
               _dialog('7', '非公開アカウントであっても、他人への誹謗中傷をSNSに投稿するのはやめましょう。法的な問題になる恐れもあります。'),
               _dialog('8', '他人の個人情報をSNSに乗せるのはやめましょう。'),
               _dialog('9', '写真には個人を特定できる情報が含まれている可能性があります。自宅の写真だけでなく、駅の近くに住んでいるなどの情報を安易に公開しないようにしましょう。'),
+              Text(''),
               Text(
                 'Web',
                 style: TextStyle(
@@ -172,11 +174,13 @@ class collection_screen extends StatelessWidget {
                   fontStyle: FontStyle.italic,
                 ),
               ),
+              Image.asset('images/nbrowser.png'),
               _dialog('10', 'ウェブサイトの中には、広告が大量に出てくるサイトもあります。広告をブロックするか、そのようなサイトにはアクセスしないようにしましょう。'),
               _dialog('11', 'URLやウェブサイトが似ていても、まったく別のページということもあります。普段からのウイルス対策が重要です。'),
               _dialog('12', '警告が出てくるなど、安全でないウェブサイトを見つけたら、絶対に開かないようにしましょう。ウイルスに感染したり、個人情報が抜き取られたりする可能性もあります。'),
               _dialog('13', 'フェイクニュースには気を付けましょう。'),
               _dialog('14', '過剰な広告が出てくるウェブは危険な可能性があります。閲覧などは避けましょう。'),
+              Text(''),
               Text(
                 'setting',
                 style: TextStyle(
@@ -185,6 +189,7 @@ class collection_screen extends StatelessWidget {
                   fontStyle: FontStyle.italic,
                 ),
               ),
+              Image.asset('images/nsettings.png'),
               _dialog('15', '「インターネット共有へ自動接続」の項目がついています。勝手に危険なネットワークに接続してしまう可能性を避けるため、できるだけ接続を切っておきましょう。'),
               _dialog('16', '利用可能なネットワークの項目に表示される、Wi-Fiの名前をSSIDといいます。例え接続ができる状態にあったとしても、知らないSSIDのネットワークには接続しないようにしましょう。'),
               _dialog('17', '安全性の低いネットワークには接続しないようにしましょう。'),
