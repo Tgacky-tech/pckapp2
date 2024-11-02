99a196f3c0f25288676803b9afe8f0b8086e9011
Tgacky-tech <shota0037.takagaki@gmail.com>
2024-11-03 00:06:02 +0900

    これでラスト

--- lib/tutorialTask_screen.dart
+++ lib/tutorialTask_screen.dart
@@ -15,7 +15,7 @@ import 'package:pckapp2/providers/scrollPosition_provider.dart';
 import 'package:pckapp2/providers/counter_provider.dart';
 import 'package:pckapp2/providers/error_provider.dart';
 import 'package:pckapp2/providers/counterList_provider.dart';
-  import 'package:pckapp2/providers/transitionFromTask_provider.dart';
+import 'package:pckapp2/providers/transitionFromTask_provider.dart';
 
 class TutorialTaskScreen extends ConsumerStatefulWidget {
   const TutorialTaskScreen({Key? key}) : super(key: key);
@@ -37,155 +37,160 @@ class _TutorialTaskScreenState extends ConsumerState<TutorialTaskScreen> {
     int n = 0;
 
     return Stack(children: [
-    Positioned.fill(
-      child: GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-      crossAxisCount: 4,// 横に並べる画像の数
-      childAspectRatio: 1.4, // 正方形として表示
-    ),
-    itemBuilder: (context, index) {
-    return FittedBox(
-    fit: BoxFit.contain, // 画像全体が見えるように縮小
-    child: Transform.rotate(
-    angle: -0.1, // 画像を斜めにする角度（ラジアンで指定）
-    child: Image.asset(
-    'images/grey.png', // 使用する画像のパス
-    fit: BoxFit.cover,
-    ),
-    ),
-    );
-    },
-    ),
-    ),
+      Positioned.fill(
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4, // 横に並べる画像の数
+            childAspectRatio: 1.4, // 正方形として表示
+          ),
+          itemBuilder: (context, index) {
+            return FittedBox(
+              fit: BoxFit.contain, // 画像全体が見えるように縮小
+              child: Transform.rotate(
+                angle: -0.1, // 画像を斜めにする角度（ラジアンで指定）
+                child: Image.asset(
+                  'images/grey.png', // 使用する画像のパス
+                  fit: BoxFit.cover,
+                ),
+              ),
+            );
+          },
+        ),
+      ),
       Scaffold(
         body: Center(
-          child: Stack(alignment: Alignment.center, children: [
-            // Center(
-            // child:level == 0 ? swipeAnimation() : tapAnimation(),
-            // ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.95,
-              height: MediaQuery.of(context).size.width * 1.8,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.circular(6),
-                  border: Border.all(color: Colors.black, width: 2),
-                ),
-                padding: EdgeInsets.all(4),
-                child: AspectRatio(
-                  aspectRatio: 27 / 51.5,
-                  child: NotificationListener<ScrollNotification>(
-                    onNotification: (ScrollNotification notification) {
-                      if (notification is ScrollUpdateNotification) {
-                        // スクロール位置をプロバイダーに更新
-                        ref.read(scrollPositionProvider.notifier).state =
-                            notification.metrics.pixels;
+          child: FittedBox(
+            fit: BoxFit.scaleDown,
+            child:
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.95,
+                height: MediaQuery.of(context).size.width * 1.8,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(6),
+                    border: Border.all(color: Colors.black, width: 2),
+                  ),
+                  padding: EdgeInsets.all(4),
+                  child: AspectRatio(
+                    aspectRatio: 27 / 51.5,
+                    child: NotificationListener<ScrollNotification>(
+                      onNotification: (ScrollNotification notification) {
+                        if (notification is ScrollUpdateNotification) {
+                          // スクロール位置をプロバイダーに更新
+                          ref.read(scrollPositionProvider.notifier).state =
+                              notification.metrics.pixels;
 
-                        // スクロール位置が100ピクセルを超えたときに画面遷移を実行
-                        if (notification.metrics.pixels > 110) {
-                          // 遷移が実行済みでない場合にのみ遷移を実行
-                          if (scrollPosition <= 110 && n == 0) {
-                            n++;
-                            if (level == 1) {
-                              ref.read(levelProvider.notifier).state++;
-                              final level = ref.read(levelProvider);
-                              ref.read(scrollPositionProvider.notifier).state =
-                                  notification.metrics.pixels;
-                              // ref.read(counterProvider.notifier).state++;
-                              // final proceedPath = '/0${ref.read(counterProvider)}';
-                              WidgetsBinding.instance.addPostFrameCallback((_) {
-                                // context.push(proceedPath);
-                                ref.read(levelProvider.notifier).state = 0;
-                                context.go('/tutorialEnd');
-                              });
+                          // スクロール位置が100ピクセルを超えたときに画面遷移を実行
+                          if (notification.metrics.pixels > 110) {
+                            // 遷移が実行済みでない場合にのみ遷移を実行
+                            if (scrollPosition <= 110 && n == 0) {
+                              n++;
+                              if (level == 1) {
+                                ref.read(levelProvider.notifier).state++;
+                                final level = ref.read(levelProvider);
+                                ref
+                                    .read(scrollPositionProvider.notifier)
+                                    .state = notification.metrics.pixels;
+                                // ref.read(counterProvider.notifier).state++;
+                                // final proceedPath = '/0${ref.read(counterProvider)}';
+                                WidgetsBinding.instance
+                                    .addPostFrameCallback((_) {
+                                  // context.push(proceedPath);
+                                  ref.read(levelProvider.notifier).state = 0;
+                                  context.go('/tutorialEnd');
+                                });
+                              }
                             }
                           }
                         }
-                      }
-                      return false; // 他のウィジェットにも通知を伝える
-                    },
-                    child: SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          Container(
-                            decoration: const BoxDecoration(
-                                image: DecorationImage(
-                              image: AssetImage('images/background2.png'),
-                              fit: BoxFit.cover,
-                            )),
-                            width: double.infinity,
-                            height: MediaQuery.of(context).size.height * 1.05,
-                            child: Center(
-                              child: Column(
-                                children: [
-                                  // Container(
-                                  //   width: MediaQuery.of(context).size.width * 0.03,
-                                  //   height: MediaQuery.of(context).size.height * 0.03,
-                                  // ),
-                                  Container(
-                                    // color: Colors.yellow,
-                                    width:
-                                        MediaQuery.of(context).size.width * 0.8,
-                                    height: MediaQuery.of(context).size.height *
-                                        0.8,
-                                    child: Container(
+                        return false; // 他のウィジェットにも通知を伝える
+                      },
+                      child: Center(child:Container(
+                              decoration: const BoxDecoration(
+                                  image: DecorationImage(
+                                image: AssetImage('images/background2.png'),
+                                fit: BoxFit.cover,
+                              )),
+                              width: double.infinity,
+                              height: MediaQuery.of(context).size.height * 1.05,
+                              child: Center(
+    child: SingleChildScrollView(child:Center(child:Container(height: MediaQuery.of(context).size.height * 2,child:Column(
+
+                                  children: [
+                                    // Container(
+                                    //   width: MediaQuery.of(context).size.width * 0.03,
+                                    //   height: MediaQuery.of(context).size.height * 0.03,
+                                    // ),
+                                    Center(child:
+                                    Container(
+                                      // color: Colors.yellow,
                                       width: MediaQuery.of(context).size.width *
                                           0.8,
                                       height:
                                           MediaQuery.of(context).size.height *
                                               0.8,
-                                      child: MaterialButton(
-                                        onPressed: () {
-                                          if (level == 0) {
-                                            ref
-                                                .read(levelProvider.notifier)
-                                                .state++;
-                                            final level =
-                                                ref.read(levelProvider);
-                                            // ref.read(counterProvider.notifier).state++;
-                                            // final proceedPath = '/0${ref.read(counterProvider)}';
-                                            WidgetsBinding.instance
-                                                .addPostFrameCallback((_) {
-                                              // context.push(proceedPath);
-                                              context.go('/tutorial');
+                                      child: Container(
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                0.8,
+                                        height:
+                                            MediaQuery.of(context).size.height *
+                                                0.8,
+                                        child: MaterialButton(
+                                          onPressed: () {
+                                            if (level == 0) {
                                               ref
-                                                  .read(
-                                                      tutorialProvider.notifier)
-                                                  .setTutorial(5);
-                                            });
-                                          }
-                                        },
-                                        child: Container(
-                                          width: MediaQuery.of(context)
-                                                  .size
-                                                  .width *
-                                              0.78,
-                                          height: MediaQuery.of(context)
-                                                  .size
-                                                  .width *
-                                              1.4,
-                                          child: Image.asset(
-                                            'images/mscreen.png',
-                                            fit: BoxFit.cover,
+                                                  .read(levelProvider.notifier)
+                                                  .state++;
+                                              final level =
+                                                  ref.read(levelProvider);
+                                              // ref.read(counterProvider.notifier).state++;
+                                              // final proceedPath = '/0${ref.read(counterProvider)}';
+                                              WidgetsBinding.instance
+                                                  .addPostFrameCallback((_) {
+                                                // context.push(proceedPath);
+                                                context.go('/tutorial');
+                                                ref
+                                                    .read(tutorialProvider
+                                                        .notifier)
+                                                    .setTutorial(5);
+                                              });
+                                            }
+                                          },
+                                          child: Container(
+                                            width: MediaQuery.of(context)
+                                                    .size
+                                                    .width *
+                                                0.78,
+                                            height: MediaQuery.of(context)
+                                                    .size
+                                                    .width *
+                                                1.4,
+                                            child: Image.asset(
+                                              'images/mscreen.png',
+                                              fit: BoxFit.cover,
+                                            ),
                                           ),
                                         ),
                                       ),
                                     ),
-                                  ),
-                                ],
+                                    ),
+                                  ],
+
                               ),
-                            ),
-                          ),
-                        ],
+                              ),
+                      ),
                       ),
                     ),
                   ),
                 ),
               ),
             ),
-          ]),
+          ),
         ),
+          ),
+      ),
       ),
       // MaterialButton(
       //   onPressed: () {
@@ -217,8 +222,7 @@ class _TutorialTaskScreenState extends ConsumerState<TutorialTaskScreen> {
                     });
                   }
                 },
-                child: tapAnimation()
-              )
+                child: tapAnimation())
             : swipeAnimation(),
       ),
       // ),
