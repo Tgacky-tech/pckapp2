f8c278b69fe385a3efeff30ee1ca59a20b2d1381
HARUKIPC\haruk <haruki14159265@gmail.com>
2024-11-03 02:00:15 +0900

    no message

--- lib/tutorial_screen.dart
+++ lib/tutorial_screen.dart
@@ -194,26 +194,27 @@ class _ScreenTutorialState extends ConsumerState<tutorial_screen>
     return Scaffold(
       body:  Stack(
         children: [
-      Positioned.fill(
-      child: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 4,// 横に並べる画像の数
-        childAspectRatio: 1.4, // 正方形として表示
-      ),
-      itemBuilder: (context, index) {
-        return FittedBox(
-          fit: BoxFit.contain, // 画像全体が見えるように縮小
-          child: Transform.rotate(
-            angle: -0.1, // 画像を斜めにする角度（ラジアンで指定）
-            child: Image.asset(
-              'images/grey.png', // 使用する画像のパス
-              fit: BoxFit.cover,
-            ),
-          ),
-        );
-      },
-    ),
-    ),Center(
+    //   Positioned.fill(
+    //   child: GridView.builder(
+    //     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+    //     crossAxisCount: 4,// 横に並べる画像の数
+    //     childAspectRatio: 1.4, // 正方形として表示
+    //   ),
+    //   itemBuilder: (context, index) {
+    //     return FittedBox(
+    //       fit: BoxFit.contain, // 画像全体が見えるように縮小
+    //       child: Transform.rotate(
+    //         angle: -0.1, // 画像を斜めにする角度（ラジアンで指定）
+    //         child: Image.asset(
+    //           'images/grey.png', // 使用する画像のパス
+    //           fit: BoxFit.cover,
+    //         ),
+    //       ),
+    //     );
+    //   },
+    // ),
+    // ),
+        Center(
         child: FittedBox(
             fit: BoxFit.scaleDown,child:SizedBox(
           width: MediaQuery.of(context).size.width * 0.95,
@@ -225,7 +226,7 @@ class _ScreenTutorialState extends ConsumerState<tutorial_screen>
               border: Border.all(color: Colors.black, width: 2),
             ),
             padding: EdgeInsets.all(4),
-            child: SingleChildScrollView(child:Container(
+            child:Container(
               decoration: const BoxDecoration(
                   image: DecorationImage(
                     image: AssetImage('images/background.png'),
@@ -373,7 +374,6 @@ class _ScreenTutorialState extends ConsumerState<tutorial_screen>
                   ),
                 ],
               ),
-            ),
           ),
         ),
         ),
